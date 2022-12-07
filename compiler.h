6a00bd903d8e619f9f85b9d1275f1b655524c80b
John Ling <johnlingbusiness@gmail.com>
2022-12-07 11:10:31 +0800

    Removed redundant dictionaries from compiler.cpp

--- compiler.h
+++ compiler.h
@@ -12,19 +12,7 @@
 class Compiler
 {    
     private:
-        const std::unordered_map<std::string, std::string> TOKEN_TABLE = {
-            {"while", "[WHILE]"}, {"output", "[OUTPUT]"}, {"input", "[INPUT]"}, {"if", "[IF]"}, {"else", "[ELSE]"},
-            {"+", "[ADDITION]"}, {"-", "[SUBTRACTION]"}, {"*", "[MULTIPLICATION]"},
-            {"/", "[DIVISION]"}, {"=", "[ASSIGNMENT]"}, {">", "[GREATER]"}, {"<", "[LESSER]"},
-            {">=", "[GREATER_EQUAL]"}, {"<=", "[LESSER_EQUAL]"}, {"==", "[EQUALS]"}, {"!=", "[NOT_EQUALS]"},
-            {"&&", "[AND]"}, {"||", "[OR]"}, {"!", "[NOT]"}, {"(", "[LBRACKET]"}, {")", "[RBRACKET]"},
-            {"[", "[LSQUARE]"}, {"]", "[RSQUARE]"}, {",", "[COMMA]"}, {";", "[SEMICOLON]"}, 
-            {"string", "[STRING]"}, {"integer", "[INTEGER]"}, {"bool", "[BOOLEAN]"}, {"end", "[END]"}
-        };
-
         std::string targetFile;
-        int begin_lexing(void);
-        void tokenize(std::string line);
     public:
         Compiler(std::string filename);
         int compile(void);
